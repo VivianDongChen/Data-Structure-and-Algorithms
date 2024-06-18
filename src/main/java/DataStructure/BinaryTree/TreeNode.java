2d6e75be237b1f7ba3c66ecb8a847c3720725db8
chendong <chen.dong1@northeastern.edu>
2024-06-17 18:44:28 -0700

    add TreeTraversal

---
+++ src/main/java/DataStructure/BinaryTree/TreeNode.java
@@ -0,0 +1,22 @@
+package DataStructure.BinaryTree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(TreeNode left, int val, TreeNode right) {
+        this.left = left;
+        this.val = val;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.val);
+    }
+}
---
+++ src/main/java/DataStructure/BinaryTree/TreeTraversal.java
@@ -0,0 +1,81 @@
+package DataStructure.BinaryTree;
+
+public class TreeTraversal {
+
+    public static void main(String[] args) {
+        /*
+          1
+        /   \
+       2     3
+      /     / \
+     4     5   6
+
+    */
+
+        TreeNode root = new TreeNode
+                (new TreeNode(new TreeNode(4),
+                        2,
+                        null),
+                        1,
+
+                        new TreeNode(new TreeNode(5),
+                                3,
+                                new TreeNode(6))
+                );
+
+        preOrder(root);
+        System.out.println();
+        inOrder(root);
+        System.out.println();
+        postOrder(root);
+        System.out.println();
+
+    }
+
+
+    /**
+     * 前序遍历 - 递归
+     * @param node 节点
+     */
+
+    static void preOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.print(node.val + "\t"); //值
+        preOrder(node.left);
+        preOrder(node.right);
+
+    }
+
+    /**
+     * 中序遍历 - 递归
+     * @param node 节点
+     */
+    static void inOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.print(node.val + "\t"); //值
+        inOrder(node.right);
+
+    }
+
+    /**
+     * 后序遍历 - 递归
+     * @param node 节点
+     */
+    static void postOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.val + "\t"); //值
+
+    }
+}
