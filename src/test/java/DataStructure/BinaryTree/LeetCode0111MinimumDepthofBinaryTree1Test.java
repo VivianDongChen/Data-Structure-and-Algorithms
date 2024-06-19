3e78af9a66fb3daa77fcf463873c919c8bca4636
chendong <chen.dong1@northeastern.edu>
2024-06-18 20:42:04 -0700

    add LeetCode 111 2#

---
+++ src/test/java/DataStructure/BinaryTree/LeetCode0111MinimumDepthofBinaryTree1Test.java
@@ -0,0 +1,33 @@
+package DataStructure.BinaryTree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeetCode0111MinimumDepthofBinaryTree1Test {
+
+    @Test
+    public void test1() {
+        TreeNode root = new TreeNode(new TreeNode(2), 1, new TreeNode(3));
+        assertEquals(2, new LeetCode0111MinimumDepthofBinaryTree1().minDepth(root));
+    }
+
+    @Test
+    public void test2() {
+        TreeNode root = new TreeNode(new TreeNode(2), 1, new TreeNode(null, 3, new TreeNode(4)));
+        assertEquals(2, new LeetCode0111MinimumDepthofBinaryTree1().minDepth(root));
+    }
+
+    @Test
+    public void test3() {
+        TreeNode root = new TreeNode(1);
+        assertEquals(1, new LeetCode0111MinimumDepthofBinaryTree1().minDepth(root));
+    }
+
+    @Test
+    public void test4() {
+        TreeNode root = new TreeNode(new TreeNode(2), 1, null);
+        assertEquals(2, new LeetCode0111MinimumDepthofBinaryTree1().minDepth(root));
+    }
+
+}
