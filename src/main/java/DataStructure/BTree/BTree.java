12a0e9e76ad6888c43edd0ed802b4db8136174ce
chendong <chen.dong1@northeastern.edu>
2024-06-26 23:50:22 -0700

    modify BTree r1

--- src/main/java/DataStructure/BTree/BTree.java
+++ src/main/java/DataStructure/BTree/BTree.java
@@ -95,7 +95,7 @@ public class BTree {
          * @return 被移除的key
          */
         int removeLeftmostKey(){
-            removeKey(0);
+            return removeKey(0);
         }
 
         /**
@@ -103,7 +103,7 @@ public class BTree {
          * @return 被移除的key
          */
         int removeRightmostKey(){
-            removeKey(keyNumber - 1);
+            return removeKey(keyNumber - 1);
         }
 
         /**
@@ -123,7 +123,7 @@ public class BTree {
          * @return 被移除的child
          */
         Node removeLeftmostChild(){
-            removeChild(0);
+            return removeChild(0);
         }
 
         /**
@@ -131,7 +131,7 @@ public class BTree {
          * @return
          */
         Node removeRightmostChild(){
-            removeChild(keyNumber);
+            return removeChild(keyNumber);
         }
 
         /**
@@ -160,7 +160,7 @@ public class BTree {
             int start = target.keyNumber;
             if(!leaf){
                 for(int i = 0; i <= keyNumber; i++){
-                    target.children[start +1] = children[i];
+                    target.children[start +i] = children[i];
                 }
             }
             for (int i = 0; i < keyNumber; i++){
@@ -252,5 +252,121 @@ public class BTree {
     }
 
     //3. 删除
+    public void remove(int key){
+        doRemove(null,root,0,key);
+    }
+
+    /**
+     * case1: 当前节点是叶子节点，没找到  => 直接返回
+     * case2: 当前节点是叶子节点，找到了 => 直接删除
+     * case3: 当前节点是非叶子节点，没找到  => 继续到孩子中查找
+     * case4: 当前节点是非叶子节点，找到了  => 找到后继的key，替换要删除的节点，删除后继key
+     * case5: 删除后key数目 < 下限（不平衡） => 进行平衡处理
+     * case6: 根节点
+     * @param node
+     * @param key
+     */
+    private void doRemove(Node parent, Node node, int index, int key) {
+        int i = 0;
+        while(i < node.keyNumber){
+            if(node.keys[i] >= key){
+                break;
+            }
+            i++;
+        }
+        //找到了：i代表删除key的索引
+        //没找到：i代表到第i个孩子中继续查找
+        if(node.leaf){
+            if(!found(node, key, i)){  //case1
+                return;
+            }else{                     //case2
+                node.removeKey(i);
+
+            }
+        }else{
+            if(!found(node, key, i)){  //case3
+                doRemove(node, node.children[i], i,key);
+            }else{                     //case4
+                //1.找到后继节点
+                Node s = node.children[i + 1];
+                while(!s.leaf){
+                    s = s.children[0];
+                }
+                int sKey = s.keys[0];
+                //2.替换待删除key
+                node.keys[i] = sKey;
+                //3. 删除后继key
+                doRemove(node,node.children[i + 1], i+1,sKey);
+
+            }
+        }
+        if(node.keyNumber < MIN_KEY_NUMBER){
+            //调整平衡  case5 case6
+            balance(parent,node, index);
+
+        }
+
+    }
+
+    /**
+     * case5，case6的调整平衡
+     * @param parent 父节点
+     * @param x 被调整节点
+     * @param i 被调整节点的索引
+     */
+    private void balance(Node parent, Node x, int i){
+        // case 6 根节点
+        if(x == root){
+            if(root.keyNumber == 0 && root.children[0] != null){
+              root = root.children[0];
+            }
+            return;
+        }
+
+        Node left = parent.childLeftSibling(i);
+        Node right = parent.childRightSibling(i);
+        // case 5-1 左边富裕，右旋
+        if(left != null && left.keyNumber > MIN_KEY_NUMBER){
+            // a) 父节点中前驱key旋转下来
+            x.insertKey(parent.keys[i-1],0);
+            // b) 如果left有孩子，将最大的节点过继给被调整节点
+            if(!left.leaf){
+                x.insertChild(left.removeRightmostChild(),0);
+            }
+            // c) left中最大的key旋转上去
+            parent.keys[i-1] = left.removeRightmostKey();
+            return;
+        }
+        // case 5-2 右边富裕，左旋
+        if(right != null && right.keyNumber > MIN_KEY_NUMBER){
+            // a) 父节点中后继key旋转下来
+            x.insertKey(parent.keys[i],x.keyNumber);
+            // b) 如果right有孩子，将最小的孩子过继给被调整节点
+            if(!right.leaf){
+                x.insertChild(right.removeLeftmostChild(),x.keyNumber);  //疑问：为什么不是x.keyNumber + 1 ？
+            }
+            // c) right中最小的key旋转上去
+            parent.keys[i] = right.removeLeftmostKey();
+            return;
+        }
+        // case 5-3 两边都不够借，向左合并
+        if(left != null){
+            // a) 有左兄弟，向左兄弟合并
+            parent.removeChild(i);
+            left.insertKey(parent.removeKey(i-1), left.keyNumber);
+            x.moveToTarget(left);
+        }else{
+            // b) 没有左兄弟，向自己合并
+            parent.removeChild(i+1);
+            x.insertKey(parent.removeKey(i), x.keyNumber);
+            right.moveToTarget(x);
+        }
+
+    }
+
+    private boolean found(Node node, int key, int i) {
+        return i < node.keyNumber && node.keys[i] == key;
+    }
+
 
 }
