32adae9d77798b7a0a50322e2944e0d026fef9e0
chendong <chen.dong1@northeastern.edu>
2024-06-28 21:11:22 -0700

    add LeetCode 49 method 3

--- src/main/java/DataStructure/HashTable/LeetCode0049GroupAnagrams.java
+++ src/main/java/DataStructure/HashTable/LeetCode0049GroupAnagrams.java
@@ -10,8 +10,15 @@ import java.util.List;
  */
 public class LeetCode0049GroupAnagrams {
 
-    /*
-        解法1 思路
+    /**
+     * 方法1
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        eat tea ate => aet
+        tan nat     => ant
+        bat         => abt
+        [(aet,{eat,tea}), (ant,{tan})]
+
+        思路
         1. 遍历字符串数组，每个字符串中的字符重新排序后作为 key
         2. 所谓分组，其实就是准备一个集合，把这些单词加入到 key 相同的集合中
         3. 返回分组结果
@@ -36,6 +43,11 @@ public class LeetCode0049GroupAnagrams {
         return new ArrayList<>(map.values());
     }
 
+    /**
+     * 方法2: 方法1的改进版
+     * @param strs
+     * @return
+     */
     public List<List<String>> groupingAnagrams2(String[] strs){
         HashMap<String, List<String>> map = new HashMap<>();
         for(String str : strs){
@@ -52,14 +64,54 @@ public class LeetCode0049GroupAnagrams {
         return new ArrayList<>(map.values());
     }
 
+     /**
+      * 方法3
+        题目中有说明：strs[i] 仅包含小写字母
+        key = [2, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]  26
+        key = [2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]  26
+        "eaat", "teaa"
+     */
+     static class ArrayKey{
+
+         int[] key = new int[26];
+
+         @Override
+         public boolean equals(Object o) {
+             if (this == o) return true;
+             if (o == null || getClass() != o.getClass()) return false;
+             ArrayKey arrayKey = (ArrayKey) o;
+             return Arrays.equals(key, arrayKey.key);
+         }
+
+         @Override
+         public int hashCode() {
+             return Arrays.hashCode(key);
+         }
+
+         public ArrayKey(String str){
+             for (int i = 0; i < str.length(); i++) {
+                 char ch = str.charAt(i); //'a'97, 'b' 98...
+                 key[ch -97] ++;
+             }
+         }
+     }
+
+     public List<List<String>> groupingAnagrams3(String[] strs){
+         HashMap <ArrayKey, List<String>> map = new HashMap<>();
+         for(String str: strs){
+             ArrayKey key = new ArrayKey(str);
+             List<String> list = map.computeIfAbsent(key, k-> new ArrayList<>());
+             list.add(str);
+         }
+         return new ArrayList<>(map.values());
+     }
+
+
+
 
     public static void main(String[] args) {
         String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-        // eat tea ate => aet
-        // tan nat     => ant
-        // bat         => abt
-        // [(aet,{eat,tea}), (ant,{tan})]
-        List<List<String>> lists = new LeetCode0049GroupAnagrams().groupingAnagrams1(strs);
+        List<List<String>> lists = new LeetCode0049GroupAnagrams().groupingAnagrams3(strs);
         System.out.println(lists);
 
 
