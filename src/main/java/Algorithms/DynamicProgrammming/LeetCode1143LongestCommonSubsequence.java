d5aca982d7d2bc21f4b2ecc4e34c28de9af4891b
chendong <chen.dong1@northeastern.edu>
2024-07-14 19:15:38 -0700

    LeetCode 1143

--- src/main/java/Algorithms/DynamicProgrammming/LeetCode1143LongestCommonSubsequence.java
+++ src/main/java/Algorithms/DynamicProgrammming/LeetCode1143LongestCommonSubsequence.java
@@ -1,5 +1,7 @@
 package Algorithms.DynamicProgrammming;
 
+import java.util.Arrays;
+
 /**
  * 最长公共子序列 - 动态规划
  */
@@ -19,4 +21,78 @@ public class LeetCode1143LongestCommonSubsequence {
         不同字符
             max(上一行, 上一列)
      */
+
+    /**
+     * 解法1: 二维数组
+     * @param text1 字符串1
+     * @param text2 字符串2
+     * @return 最长公共子序列
+     */
+    public int longestCommonSubsequence1(String text1, String text2){
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m+1][n+1]; //将行和列都比数组长度多一个，这样方便处理i-1和j-1；
+        for (int i = 1; i < m+1; i++) {
+            for (int j = 1; j < n+1; j++) {
+                if(text1.charAt(i-1) == text2.charAt(j-1)){      //两个数组要从索引0开始检查
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        print(dp, text2, text1);
+
+        return dp[m][n];
+
+    }
+
+    /**
+     * 解法2: 一维数组
+     * @param text1 字符串1
+     * @param text2 字符串2
+     * @return 最长公共子序列
+     */
+    public int longestCommonSubsequence2(String text1, String text2){
+        int m = text1.length();
+        int n = text2.length();
+        int[] dp = new int[n+1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n+1; j++) {
+                if(text1.charAt(i) == text2.charAt(j-1)){
+                    dp[j] = dp[j-1] + 1;
+                }else{
+                    dp[j] = Math.max(dp[j], dp[j-1]);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+
+        return dp[n];
+
+    }
+
+    public static void main(String[] args) {
+        LeetCode1143LongestCommonSubsequence test = new LeetCode1143LongestCommonSubsequence();
+        System.out.println(test.longestCommonSubsequence2("abxyz", "abcxyz"));
+        System.out.println(test.longestCommonSubsequence1("ba", "yby"));
+    }
+
+    /**
+     * 打印二维数组
+     * @param dp 二维数组
+     * @param a 字符串1 （列）
+     * @param b 字符串2 （行）
+     */
+    static void print(int[][] dp, String a, String b) {
+        System.out.println("-".repeat(23));
+        Object[] array = a.chars().mapToObj(i -> String.valueOf((char) i)).toArray();
+        System.out.printf("     " + "%2s ".repeat(a.length()) + "%n", array);
+        System.out.printf("     " + "%2s ".repeat(a.length()) + "%n", a.chars().mapToObj(i -> "0").toArray());
+        for (int i = 0; i < b.length(); i++) {
+            int[] d = dp[i + 1];
+            array = Arrays.stream(d).boxed().toArray();
+            System.out.printf(b.charAt(i) + " " + "%2d ".repeat(d.length) + "%n", array);
+        }
+    }
 }
