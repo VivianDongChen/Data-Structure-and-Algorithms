0aed74cdc05a09212232e42e5288746f890f9545
chendong <chen.dong1@northeastern.edu>
2024-07-21 00:08:56 -0700

    LeetCode 28

---
+++ src/main/java/Algorithms/TwoPointers/LeetCode0028FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,30 @@
+package Algorithms.TwoPointers;
+
+/**
+ * 字符串匹配 - 两个指针（暴力匹配）
+ */
+public class LeetCode0028FindTheIndexOfTheFirstOccurrenceInAString {
+    static int strStr(String str1, String str2){
+        //转化为字符数组，以便于遍历
+        char[] origin = str1.toCharArray();
+        char[] pattern = str2.toCharArray();
+        int i = 0;
+        while (i <= origin.length - pattern.length) {
+            int j;
+            for (j = 0; j < pattern.length; j++){
+                if(origin[i+j] != pattern[j]){
+                    break;
+                }
+            }
+            if(j == pattern.length){
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(strStr("aaacaaab", "aaab"));
+    }
+}
