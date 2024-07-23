f6961ad530de8c9c9d2ac26aa94f62d057de6541
chendong <chen.dong1@northeastern.edu>
2024-07-23 00:19:39 -0700

    modify LeetCode 28 -KMP

--- src/main/java/Algorithms/KnuthMorrisPratt/LeetCode0028FindTheIndexOfTheFirstOccurrenceInAStringKMP.java
+++ src/main/java/Algorithms/KnuthMorrisPratt/LeetCode0028FindTheIndexOfTheFirstOccurrenceInAStringKMP.java
@@ -10,7 +10,7 @@ public class LeetCode0028FindTheIndexOfTheFirstOccurrenceInAStringKMP {
     static int strStr(String str1, String str2){
         char[] origin = str1.toCharArray();
         char[] pattern = str2.toCharArray();
-        int[] lsp = lsp(pattern);
+        int[] lsp = lps(pattern);
         int i = 0;
         int j = 0;
         /*
@@ -26,7 +26,7 @@ public class LeetCode0028FindTheIndexOfTheFirstOccurrenceInAStringKMP {
             }else if( j == 0){ //j还没有往前走，就发现不匹配
                 i++;
             }else{
-                j = lsp[j-1]; //数组lsp的值对应最长前后缀的长度，索引对应的是j-1
+                j = lsp[j-1]; //索引j-1对应的值就是此时最长前后缀的长度
             }
             if(j == pattern.length){  //找到解
                 return i - j;
@@ -37,24 +37,81 @@ public class LeetCode0028FindTheIndexOfTheFirstOccurrenceInAStringKMP {
 
 
     /**
-     * 求数组的最长前后缀
-     * @param pattern
-     * @return
+     * 求数组的最长前后缀（longest prefix suffix ）
+     * @param pattern 数组
+     * @return 最长前后缀数组
      */
-    static int[] lsp(char[] pattern){
+    static int[] lps(char[] pattern){
         /*
-           匹配失败后j无需跳转到0， 而是跳转到前面匹配成功的字符串的最长公共子串的后面的位置（这个公共子串是origin的后缀和pattern的前缀），
-           使用一个数组来存放这个公共子串的数据：
-           最长后缀数组：只跟pattern字符串有关
+           匹配失败后j无需跳转到0， 而是跳转到前面匹配成功的字符串的最长公共子串的后面的位置（这个公共子串是origin的后缀和pattern的前缀， 也就是pattern的前后缀），
+           使用一个数组来存放这个子串的数据：
+
+           最长前后缀数组：只跟pattern字符串有关
            1. 索引： 使用了pattern字符串前j个字符串 - 1
-           2。 值：最长前后缀的长度（恰好是匹配失败后j要跳转的位置））
+           2. 值：最长前后缀的长度（恰好是匹配失败后j要跳转的位置））
+
+           如何找一个数组的最长前后缀：
+
+           1.将一个数组复制一份， 这样排列：
+
+                       j
+                    a  a  a  d  e  f
+                 a  a  a  d  e  f
+                       i
+
+             对于d来讲， aaa是它前面的字符串，aa是这个字符串的前缀，aa是这个字符串的后缀, 把这个字符串的长度（j+1）记录到lsp[i]， 然后j++， i++
+
+
+            2. 如果 pattern[i] != pattern[j]
+
+                2.1 j = 0, i++
+
+                2.2 j!= 0  j回到它对应的最长前后缀之后的位置， 然后再与i进行比较
+
+                          j
+                    a  a  a  d  e  f
+                 a  a  a  d  e  f
+                          i
+
+                    j = 2 lsp[2-1] = 1
+
+                       j
+                    a  a  a  d  e  f
+                 a  a  a  d  e  f
+                          i
+
+                   j继续后退  j = 1 lsp[0] = 0
+
+                    j
+                    a  a  a  d  e  f
+                 a  a  a  d  e  f
+                          i
+
+                   这个时候说明e之前的字串没有共同前后缀，i++
+
         */
-        return new int[]{0,0,1,2,3,0,1};  //假设已经求出了这个数组
+        int[] lps = new int[pattern.length];    //lsp[0] = 0;
+        int i = 1;
+        int j = 0;
+        while(i < pattern.length){
+            if(pattern[i] == pattern[j]){
+                lps[i] = j + 1;
+                i++;
+                j++;
+            }else if(j == 0){
+                i++;
+            }else{
+                j = lps[j-1];
+            }
+
+        }
+        return lps;
+
     }
 
     public static void main(String[] args) {
         System.out.println(strStr("ababababcabacacababaca", "ababaca"));
         System.out.println("ababababcabacacababaca".indexOf("ababaca"));
-//        System.out.println(Arrays.toString(lps("ababaca".toCharArray())));
+        System.out.println(Arrays.toString(lps("ababaca".toCharArray())));
     }
 }
