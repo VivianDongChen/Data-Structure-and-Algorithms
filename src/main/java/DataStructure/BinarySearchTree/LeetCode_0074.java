53e9ede7543ebdf9636a6e5d7badf350b0f4163e
VivianDongChen <chen.dong1@northeastern.edu>
2025-01-11 19:46:25 -0800

    leetcode 0074

---
+++ src/main/java/DataStructure/BinarySearchTree/LeetCode_0074.java
@@ -0,0 +1,43 @@
+package DataStructure.BinarySearchTree;
+
+/**
+ * 74. Search a 2D Matrix
+ */
+public class LeetCode_0074 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length;
+        int column = matrix[0].length;
+
+        int end = row * column - 1;
+        int start = 0;
+
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            int m = mid / column;
+            int n = mid % column;
+            if(target == matrix[m][n]){
+                return true;
+            }else if(target > matrix[m][n]){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        LeetCode_0074 test = new LeetCode_0074();
+
+        int[][] matrix = new int[][]{
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60}
+        };
+
+        int target = 3;
+
+        System.out.println(test.searchMatrix(matrix, target));
+
+    }
+}
