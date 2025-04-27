cb98d0922f90b2d895c48a4ac85a640f4c7e6ca3
VivianDongChen <chen.dong1@northeastern.edu>
2025-04-27 13:07:04 -0700

    AdjacencyListGraph

---
+++ src/main/java/DataStructure/Graph/AdjacencyListGraph.java
@@ -0,0 +1,23 @@
+package DataStructure.Graph;
+
+import java.util.LinkedList;
+
+public class AdjacencyListGraph {
+    private int numVertices;
+    private LinkedList<Integer>[] adjLists;
+
+    //Constructor
+    public AdjacencyListGraph(int numVertices) {
+        this.numVertices = numVertices;
+        adjLists = new LinkedList[numVertices];
+        for (int i = 0; i < numVertices; i++) {
+            adjLists[i] = new LinkedList<>();
+        }
+    }
+
+    //add edge
+    public void addEdge(int src, int dest){
+        adjLists[src].add(dest);
+        adjLists[dest].add(src);
+    }
+}
